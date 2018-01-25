03933244a26889ee36fa4a8bba2b8dc9861990d3
Evgeny Kharitonov <kharitonov@fb.com>
2018-01-25 11:48:53 +0000

    [UT] UT for Adagrad
    - Generalized SGD XOR test to accept a model and an optimizer as params
    - Made it test SGD, Adagrad, and Adam

--- test.cpp
+++ test.cpp
@@ -104,6 +104,37 @@ class CartPole {
     };
 };
 
+void test_optimizer_xor(Optimizer optim, std::shared_ptr<ContainerList> model) {
+   float running_loss = 1;
+   int epoch = 0;
+   while (running_loss > 0.1) {
+     auto bs = 4U;
+     auto inp = at::CPU(at::kFloat).tensor({bs, 2});
+     auto lab = at::CPU(at::kFloat).tensor({bs});
+     for (auto i = 0U; i < bs; i++) {
+       auto a = std::rand() % 2;
+       auto b = std::rand() % 2;
+       auto c = a ^ b;
+       inp[i][0] = a;
+       inp[i][1] = b;
+       lab[i] = c;
+     }
+     // forward
+     auto x = Var(inp);
+     auto y = Var(lab, false);
+     for (auto layer : *model) x = layer->forward({x})[0].sigmoid_();
+     Variable loss = at::binary_cross_entropy(x, y);
+
+     optim->zero_grad();
+     backward(loss);
+     optim->step();
+
+     running_loss = running_loss * 0.99 + loss.data().sum().toCFloat() * 0.01;
+     EXPECT(epoch < 3000);
+     epoch++;
+   }
+ };
+
 std::map<std::string, std::function<void()>> construct_tests() {
  std::map<std::string, std::function<void()>> tests;
 
@@ -349,43 +380,33 @@ std::map<std::string, std::function<void()>> construct_tests() {
  };
 
  tests["autograd/optim/sgd"] = []() {
-   // We better be able to learn XOR
    auto model = ContainerList()
      .append(Linear(2, 8).make())
      .append(Linear(8, 1).make())
      .make();
 
    auto optim = SGD(model, 1e-1).momentum(0.9).nesterov().weight_decay(1e-6).make();
+   test_optimizer_xor(optim, model);
+ };
 
-   float running_loss = 1;
-   int epoch = 0;
-   while (running_loss > 0.1) {
-     auto bs = 4U;
-     auto inp = at::CPU(at::kFloat).tensor({bs, 2});
-     auto lab = at::CPU(at::kFloat).tensor({bs});
-     for (auto i = 0U; i < bs; i++) {
-       auto a = std::rand() % 2;
-       auto b = std::rand() % 2;
-       auto c = a ^ b;
-       inp[i][0] = a;
-       inp[i][1] = b;
-       lab[i] = c;
-     }
-     //
-     // forward
-     auto x = Var(inp);
-     auto y = Var(lab, false);
-     for (auto layer : *model) x = layer->forward({x})[0].sigmoid_();
-     Variable loss = at::binary_cross_entropy(x, y);
+ tests["autograd/optim/adagrad"] = []() {
+   auto model = ContainerList()
+     .append(Linear(2, 8).make())
+     .append(Linear(8, 1).make())
+     .make();
 
-     optim->zero_grad();
-     backward(loss);
-     optim->step();
+   auto optim = Adagrad(model, 1.0).weight_decay(1e-6).lr_decay(1e-3).make();
+   test_optimizer_xor(optim, model);
+ };
 
-     running_loss = running_loss * 0.99 + loss.data().sum().toCFloat() * 0.01;
-     EXPECT(epoch < 3000);
-     epoch++;
-   }
+ tests["autograd/optim/adam"] = []() {
+   auto model = ContainerList()
+     .append(Linear(2, 8).make())
+     .append(Linear(8, 1).make())
+     .make();
+
+   auto optim = Adam(model, 1.0).weight_decay(1e-6).make();
+   test_optimizer_xor(optim, model);
  };
 
  tests["autograd/serialization/undefined"] = []() {
