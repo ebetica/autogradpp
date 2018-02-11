5acbf78d5790700f71d769c807bd29b94b3e3ad5
Evgeny Kharitonov <kharitonov@fb.com>
2018-02-10 23:21:27 -0800

    Adding RMSprop optimizer + UTs
    Follows the pytorch implementation.

--- autograd.cpp
+++ autograd.cpp
@@ -422,6 +422,59 @@ void Adagrad::init_state() {
   step_.clear();
 }
 
+/// Adapted from
+/// https://github.com/pytorch/pytorch/blob/master/torch/optim/rmsprop.py
+void RMSprop::step() {
+  for (auto& pair : model_->parameters()) {
+    auto& name = pair.first;
+    auto& grad = pair.second.grad();
+    auto& p = pair.second.data();
+    if (!grad.defined())
+      continue;
+
+    if (square_avg_buffer_.find(name) == square_avg_buffer_.end()) {
+      square_avg_buffer_[name] = at::zeros_like(p);
+      if (momentum_) {
+        momentum_buffer_[name] = at::zeros_like(p);
+      };
+      if (centered_) {
+        grad_avg_buffer_[name] = at::zeros_like(p);
+      };
+    };
+
+    auto d_p = grad.data();
+    if (weight_decay_ > 0) {
+      d_p.add_(p, weight_decay_);
+    };
+
+    auto& square_avg = square_avg_buffer_[name];
+    square_avg.mul_(alpha_).addcmul_(d_p, d_p, 1.0 - alpha_);
+
+    at::Tensor avg;
+    if (centered_) {
+      auto& grad_avg = grad_avg_buffer_[name];
+      grad_avg.mul_(alpha_).add_(d_p, 1.0 - alpha_);
+      avg = square_avg.addcmul(grad_avg, grad_avg, -1.0).sqrt().add_(eps_);
+    } else {
+      avg = square_avg.sqrt().add_(eps_);
+    };
+
+    if (momentum_ > 0) {
+      auto& buf = momentum_buffer_[name];
+      buf.mul_(momentum_).addcdiv_(d_p, avg);
+      p.add_(buf, -lr_);
+    } else {
+      p.addcdiv_(d_p, avg, -lr_);
+    };
+  }
+}
+
+void RMSprop::init_state() {
+  square_avg_buffer_.clear();
+  momentum_buffer_.clear();
+  grad_avg_buffer_.clear();
+}
+
 void Adam::step() {
   for (auto& pair : model_->parameters()) {
     auto& name = pair.first;
